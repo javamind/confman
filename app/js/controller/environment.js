42e0b83a3fb0c37973525e0809a13650e8907a9d
ehret_g <guillaume.ehret@gmail.com>
2014-07-22 23:55:53 +0200

    Modif ecran environment

--- app/js/controller/environment.js
+++ app/js/controller/environment.js
@@ -2,12 +2,34 @@
  * Controller linked to the env list
  */
 confman.controller('environmentCtrl', function ($rootScope, $scope, $materialDialog, $location, Environment) {
+    //delete an environment
     var delete_env =  function (){
-        //dialog();
-        alert($scope.envToDelete)
+        //We read the env to delete
+        var eltToDelete = [];
+        var c=0;
+        $scope.environments.forEach(function (element) {
+            if(element.todelete){
+                eltToDelete[c++] = element;
+            }
+        });
+        if(eltToDelete.length>0){
+            $scope.todelete= {
+                entity : 'environments'
+            };
+            $materialDialog({
+                templateUrl: 'dialog/dialog-confirm-delete.html',
+                controller: ['$scope', '$hideDialog', function($scope, $hideDialog) {
+                    $scope.close = function() {
+                        $hideDialog();
+                    };
+                }]
+            });
+
+        }
+
     };
 
-
+    //Page definition
     $rootScope.currentPage = {
         name : 'Environment',
         actionbar : [
@@ -19,23 +41,31 @@ confman.controller('environmentCtrl', function ($rootScope, $scope, $materialDia
     };
 
     //Load environments
-    $scope.environments = Environment.query();
-    //Init env selected for deletion
-    $scope.environments.forEach(function logArrayElements(element, index) {
-        $scope.envToDelete.envId = true;
+    $scope.environments = Environment.query({}, function(elements){
+        //Init env selected for deletion
+        elements.forEach(function (element) {
+            element.todelete = false;
+        });
     });
 
 
-    $scope.update_env =  function (id){
-        $location.path('/environment/'+id);
+    //update an environment
+    $scope.update_env =  function (elt){
+        //$location.path('/environment/'+id);
+        $scope.entity = {
+            verb : 'Update environment',
+            content : elt
+        };
     };
+
+
 
 
 })
 
 confman.controller('environmentDetailCtrl', function ($rootScope, $scope,$routeParams) {
     $rootScope.currentPage = {
-        name : "Detail Environment",
+        name : 'Detail Environment',
         actionbar : []
     };
 

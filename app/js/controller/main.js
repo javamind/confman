ac5319dd8ec7fc96144bd6ff3b3a47d5a65a3fa3
“Guillaume <guillaume.ehret.java@gmail.com>
2014-09-19 17:37:50 +0200

    Control on index if server not respond

--- app/js/controller/main.js
+++ app/js/controller/main.js
@@ -8,10 +8,14 @@ angular.module('confman').controller('MainCtrl', function ($rootScope, $scope, $
         actionbar : []
     };
 
-    $scope.refresh = function(){
-        $http.get(constants.urlserver + 'hello/test')
-            .success(function (data) {
-                $scope.myname = "data";
-            });
-    }
+
+    $http.get(constants.urlserver + 'environment')
+        .success(function (data) {
+            $scope.errorUrl = "";
+        })
+        .error(function () {
+            $scope.errorUrl = "Impossible to dialog with confman server. Verify the server port in the config file [CONFMAN_PATH]/app/config.js";
+        });
+
+
 })
